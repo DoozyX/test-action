b865e4b058152608993e66c132cae2cbb3c58297
Doozy <slobodan_kletnikov@hotmail.com>
2019-10-19 13:43:47 +0200

    test

--- .github/actions/test-clang-format/entrypoint.sh
+++ .github/actions/test-clang-format/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -l
 
+echo "$GITHUB_WORKSPACE"
+echo "${INPUT_SOURCE}"
+echo "${INPUT_EXCLUDE}"
+echo "${INPUT_EXTENSIONS}"
+
+echo "/run-clang-format.py -r ${INPUT_SOURCE} --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS}"
+
 cd "$GITHUB_WORKSPACE"
 /run-clang-format.py -r ${INPUT_SOURCE} --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS}
