bedd198094ef1ff3a54718247c971d6e3d50d9c8
Doozy <slobodan_kletnikov@hotmail.com>
2019-12-07 20:54:55 +0100

    5

--- .github/actions/test-clang-format/entrypoint.sh
+++ .github/actions/test-clang-format/entrypoint.sh
@@ -2,7 +2,5 @@
 
 cd "$GITHUB_WORKSPACE"
 
-echo $GITHUB_WORKSPACE
-echo ${INPUT_CLANGFORMATVERSION}
 ln -s /clang-format/clang-format${INPUT_CLANGFORMATVERSION} /usr/bin/clang-format
 /run-clang-format.py -r --exclude ${INPUT_EXCLUDE} --extensions ${INPUT_EXTENSIONS} ${INPUT_SOURCE}
